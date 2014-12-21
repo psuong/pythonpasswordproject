4d7d739243052f44c800254fc0571d779021ebcf
Porrith Suong <psuong95@gmail.com>
2014-12-21 11:15:32 -0500

    Version 1: Reading through the Yahoo File. Version 2: Fixed the writing error. -Note: The Yahoo file had no encryption it was simply reading through a text file."

--- yahoo.py
+++ yahoo.py
@@ -1,13 +1,18 @@
 import re
 
 yahooList = open("Yahoo.txt", "r")
+#allows the user to name his/her file
 #fname = raw_input("Enter a name for the file: ")
 fname = "yahoocracked.txt"
 newFile = open(fname, "w")
 newFile.write("Line #\tEmail\tPassword")
 emailRegex = r'[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|edu|net)'
+index = 0
 with open("Yahoo.txt") as emailList:
     for line in emailList:
-        emailList = yahooList.readlines()
-        emailResult = re.search(emailRegex, emailList)
-        newFile.write("".join(emailList))
+        index += 1
+        emailList = yahooList.readlines(1)
+        string = "".join(emailList)
+        emailResult = re.search(emailRegex, line)
+        if emailResult:
+            newFile.write(line)

96c6182d98b41863363bcefa9c97b4d05cadfc24
Porrith Suong <psuong95@gmail.com>
2014-12-21 16:29:58 -0500

    Build 1: Reading through the Yahoo.txt file/ Build 2: Bruteforcing through the Yahoo.txt and writing it to another file.  Has general function to find emails and passwords. Build 2.1: Removed useless pieces of code, fixed the file extension problem.

--- yahoo.py
+++ yahoo.py
@@ -3,18 +3,18 @@ import re
 def bruteForce():
     yahooList = open("Yahoo.txt", "r")
     #allows the user to name his/her file
-    fname = raw_input("Enter a name for the file (this will have .txt extension): ")
-    fname.join((fname,'.txt'))
-    #fname = "yahoocracked.txt"
+    input = raw_input("Enter a name for the file (this will have .txt extension): ")
+    fname = ''.join((input, '.txt'))
+    #fname = "yahoocracked.txt
     newFile = open(fname, "w")
     newFile.write("This is the format of the file:\nLine #\tEmail\tPassword\n\n")
+    #general case for emails
     emailRegex = r'[0-9]:+[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]'
     index = 0
 
     with open("Yahoo.txt") as emailList:
         for line in emailList:
             index += 1
-            emailList = yahooList.readlines(1)
             emailResult = re.search(emailRegex, line)
             if emailResult:
                 string = line [line.index(':')::]

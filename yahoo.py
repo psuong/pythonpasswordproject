5c597c3b351c3c479276ffa900a7bd24bbc186b6
Porrith Suong <psuong95@gmail.com>
2014-12-21 15:23:21 -0500

    Build 1: Reading through the Yahoo password file. Build 2: Added functionality to find general emails, line numbers, and write to another file.

--- yahoo.py
+++ yahoo.py
@@ -1,18 +1,29 @@
 import re
 
-yahooList = open("Yahoo.txt", "r")
-#allows the user to name his/her file
-#fname = raw_input("Enter a name for the file: ")
-fname = "yahoocracked.txt"
-newFile = open(fname, "w")
-newFile.write("Line #\tEmail\tPassword")
-emailRegex = r'[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|edu|net)'
-index = 0
-with open("Yahoo.txt") as emailList:
-    for line in emailList:
-        index += 1
-        emailList = yahooList.readlines(1)
-        string = "".join(emailList)
-        emailResult = re.search(emailRegex, line)
-        if emailResult:
-            newFile.write(line)
+def bruteForce():
+    yahooList = open("Yahoo.txt", "r")
+    #allows the user to name his/her file
+    fname = raw_input("Enter a name for the file (this will have .txt extension): ")
+    fname.join((fname,'.txt'))
+    #fname = "yahoocracked.txt"
+    newFile = open(fname, "w")
+    newFile.write("This is the format of the file:\nLine #\tEmail\tPassword\n\n")
+
+    emailRegex = r'[0-9]:+[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]'
+    numRegex = r'[0-9]:'
+    index = 0
+
+    with open("Yahoo.txt") as emailList:
+        for line in emailList:
+            index += 1
+            emailList = yahooList.readlines(1)
+            emailResult = re.search(emailRegex, line)
+            if emailResult:
+                string = line [line.index(':')::]
+                lineNum = str(index)
+                accountInfo = ''.join((lineNum, string))
+                newFile.write(accountInfo)
+    yahooList.close()
+    newFile.close()
+
+bruteForce()
